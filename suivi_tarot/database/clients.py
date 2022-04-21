0ec7dbb1b68950fd27db76fec6a1f82bd033c1f9
rom4in_n <romain.navarro38@gmail.com>
2022-04-21 17:26:11 +0200

    Add 2 functions for window SelectDates

--- suivi_tarot/database/clients.py
+++ suivi_tarot/database/clients.py
@@ -1,4 +1,6 @@
-from sqlalchemy import select, update, and_
+import datetime
+
+from sqlalchemy import select, update, and_, func
 
 import suivi_tarot.database.models as md
 from suivi_tarot.api.utils import DATA_FILE
@@ -12,12 +14,14 @@ def init_bdd():
     insert_new_player({'nickname': 'Chien', 'active': False, 'protect': True})
     insert_new_player({'nickname': 'Solo', 'active': False, 'protect': True})
 
+
 def insert_new_partie(**partie) -> int:
     """Insère les données d'une session ainsi que les donnes associées"""
     partie = md.Partie(**partie)
     md.session.add(partie)
     md.session.commit()
     return partie.id
+
 
 def insert_players_partie(partie_id: int, players: list[str]):
     """Insère l'id des joueurs ayant participé à une partie dans la
@@ -28,12 +32,14 @@ def insert_players_partie(partie_id: int, players: list[str]):
         md.session.add(partie_player)
     md.session.commit()
 
+
 def insert_donne(donne: md.Donne) -> int:
     """Insère une donne dans la bdd et retourne son id
     généré automatiquement"""
     md.session.add(donne)
     md.session.commit()
     return donne.id
+
 
 def insert_preneur(donne_id: int, nickname: str):
     """Enregistre en bdd l'id du joueur ayant participé à une donne
@@ -42,12 +48,14 @@ def insert_preneur(donne_id: int, nickname: str):
     md.session.add(md.Preneur(donne_id=donne_id, player_id=joueur_id))
     md.session.commit()
 
+
 def insert_appele(donne_id: int, nickname: str):
     """Enregistre en bdd l'id du joueur ayant participé à une donne
     comme appele (partie à 5 ou 6 joueurs uniquement)"""
     player_id = get_player_id(nickname)
     md.session.add(md.Appele(donne_id=donne_id, player_id=player_id))
     md.session.commit()
+
 
 def insert_pnj(donne_id: int, nickname: str):
     """Enregistre en bdd l'id du joueur ayant participé à une donne
@@ -56,6 +64,7 @@ def insert_pnj(donne_id: int, nickname: str):
     md.session.add(md.Pnj(donne_id=donne_id, player_id=player_id))
     md.session.commit()
 
+
 def insert_defense(donne_id: int, nickname: str, number: int):
     """Enregistre en bdd l'id du joueur ayant participé à une donne
     comme defenseur"""
@@ -63,11 +72,13 @@ def insert_defense(donne_id: int, nickname: str, number: int):
     md.session.add(md.Defense(donne_id=donne_id, player_id=player_id, number=number))
     md.session.commit()
 
+
 def get_player_id(nickname: str) -> int:
     """Retourne l'id d'un joueur en fonction de son pseudo"""
     statement = select(md.Player.id).where(md.Player.nickname == nickname)
     player_id = md.session.execute(statement).first()
     return player_id[0]
+
 
 def insert_new_player(player: dict):
     """Insertion en bdd d'un joueur. Dictionnaire du type :
@@ -75,10 +86,12 @@ def insert_new_player(player: dict):
     md.session.add(md.Player(**player))
     md.session.commit()
 
+
 def get_all_players() -> list[str]:
     """Retourne la liste de tous les joueurs"""
     players = md.session.execute(select(md.Player.nickname))
     return players.scalars().all()
+
 
 def get_active_players() -> list[str]:
     """Retourne la liste des joueurs actifs"""
@@ -86,26 +99,46 @@ def get_active_players() -> list[str]:
     players = md.session.execute(statement)
     return players.scalars().all()
 
+
 def get_inactive_players() -> list[str]:
     """Retourne la liste des joueurs inactifs"""
     statement = select(md.Player.nickname).where(
-                        and_(
-                            md.Player.active == False,
-                            md.Player.protect == False
-                        ))
+        and_(
+            md.Player.active == False,
+            md.Player.protect == False
+        ))
     players = md.session.execute(statement)
     return players.scalars().all()
+
 
 def update_status_joueurs(status_player: dict):
     """Met à jour le champ actif d'un ou plusieurs joueurs
     à partir d'un dictionnaire du type {"pseudo": bool}"""
     for nickname, status in status_player.items():
-        statement = update(md.Player).where(md.Player.nickname == nickname).values(active=status).\
+        statement = update(md.Player).where(md.Player.nickname == nickname).values(active=status). \
             execution_options(synchronize_session='fetch')
         md.session.execute(statement)
     md.session.commit()
 
 
+def get_distinct_years() -> list[str]:
+    """Retourne la liste des années en ordre decroissant où au moins une
+    partie a été jouée"""
+    from sqlalchemy import extract, desc
+
+    years = md.session.query(extract('year', md.Partie.date_).label('year')) \
+        .distinct().order_by(desc('year'))
+    return [str(year[0]) for year in years]
+
+
+def get_min_max_dates_parties() -> tuple[datetime.datetime | None, datetime.datetime | None]:
+    """Retourne les dates, datetime, de la première et de la dernière partie enregistée.
+    Si la bdd est vide, retourne un tuple de None."""
+    query = md.session.query(func.min(md.Partie.date_), func.max(md.Partie.date_)).all()
+    return query[0][0], query[0][1]
+
+
 if __name__ == '__main__':
     if not DATA_FILE.exists():
         init_bdd()
+    print(get_min_max_dates_parties())
