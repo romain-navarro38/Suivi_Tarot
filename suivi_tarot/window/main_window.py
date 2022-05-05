7354c7317ac006a6fc481fd750018171c1781a4b
rom4in_n <romain.navarro38@gmail.com>
2022-05-05 23:02:23 +0200

    Add ranking feature

--- suivi_tarot/window/main_window.py
+++ suivi_tarot/window/main_window.py
@@ -1,6 +1,7 @@
 from PySide6.QtCore import Qt, QSize
 from PySide6.QtWidgets import QWidget, QGroupBox, QPushButton, QHBoxLayout, QVBoxLayout
 
+from suivi_tarot.window.ranking import RankingWindow
 from suivi_tarot.window.select_joueur import SelectPlayerWindow
 from suivi_tarot.window.gestion_joueur import ManagementPlayerWindow
 from suivi_tarot.window.table import LabelScore
@@ -32,7 +33,7 @@ class MainWindow(QWidget):
     def create_widgets(self):
         self.gb_play = QGroupBox(self)
         self.btn_play = CustomButton("Jouer")
-        self.btn_classement = CustomButton("Classement\ngénéral")
+        self.btn_ranking = CustomButton("Classement\ngénéral")
         self.btn_statistics = CustomButton("Statistique")
 
         self.gb_gestion = QGroupBox(self)
@@ -50,7 +51,7 @@ class MainWindow(QWidget):
 
     def add_widgets_to_layouts(self):
         self.play_layout.addWidget(self.btn_play)
-        self.play_layout.addWidget(self.btn_classement)
+        self.play_layout.addWidget(self.btn_ranking)
         self.play_layout.addWidget(self.btn_statistics)
         self.main_layout.addWidget(self.gb_play)
 
@@ -60,6 +61,7 @@ class MainWindow(QWidget):
 
     def setup_connections(self):
         self.btn_play.clicked.connect(self.new_partie)
+        self.btn_ranking.clicked.connect(self.display_ranking)
         self.btn_player.clicked.connect(self.management_player)
 
     def new_partie(self):
@@ -67,6 +69,10 @@ class MainWindow(QWidget):
         self.partie = SelectPlayerWindow()
         self.partie.setWindowModality(Qt.ApplicationModal)
         self.partie.show()
+
+    def display_ranking(self):
+        self.ranking = RankingWindow()
+        self.ranking.show()
 
     def management_player(self):
         self.manage_player = ManagementPlayerWindow(self)
