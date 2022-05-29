18a4d6af0382bc52030fd5e19fd3290ec77dab34
rom4in_n <romain.navarro38@gmail.com>
2022-05-29 11:13:19 +0200

    Add functions check path and password

--- suivi_tarot/api/verification.py
+++ suivi_tarot/api/verification.py
@@ -1,16 +1,18 @@
-from enum import Enum, auto
+from enum import Enum
+from pathlib import Path
 
-from suivi_tarot.database.clients import get_all_players
+from suivi_tarot.api.utils import hashage_password
+from suivi_tarot.database.clients import get_all_players, get_hash_and_salt
 
 
 class Comparison(Enum):
     """Enumération des opérateurs de comparaison"""
-    EQUAL = auto()
-    DIFFERENT = auto()
-    GREATER_THAN = auto()
-    LESS_THAN = auto()
-    GREATER_THAN_OR_EQUAL = auto()
-    LESS_THAN_OR_EQUAL = auto()
+    EQUAL = 1
+    DIFFERENT = 2
+    GREATER_THAN = 3
+    LESS_THAN = 4
+    GREATER_THAN_OR_EQUAL = 5
+    LESS_THAN_OR_EQUAL = 6
 
 
 def player_is_valid(new_player: str) -> bool:
@@ -19,11 +21,13 @@ def player_is_valid(new_player: str) -> bool:
     available = check_availability_nickname(new_player)
     return all([length, available])
 
+
 def check_availability_nickname(new_player: str) -> bool:
     """Vérifie si le pseudo est disponible dans la bdd
     (non sensible à la casse)"""
     existing_players = get_all_players()
     return new_player.lower() not in [player.lower() for player in existing_players]
+
 
 def check_length_str(text: str, target: int, comparison: Comparison) -> bool:
     """Compare la longueur d'une chaîne de caractères à une cible en fonction
@@ -43,5 +47,19 @@ def check_length_str(text: str, target: int, comparison: Comparison) -> bool:
             return len(text) <= target
 
 
+def path_is_existing_folder_or_file(path: str, type_: str) -> bool:
+    """Vérifie si un chemin et bien un dossier ou un fichier"""
+    return Path(path).is_dir() if type_ == "folder" else Path(path).is_file()
+
+
+def check_password(password: str) -> bool:
+    """Contrôle du mot de passe"""
+    hash_, salt = get_hash_and_salt()
+    password_hash, _ = hashage_password(password, salt)
+    if password_hash == hash_:
+        return True
+    return False
+
+
 if __name__ == '__main__':
-    print(check_length_str("ee", 2, Comparison.EQUAL))
+    print(path_is_existing_folder_or_file(r"C:\Users\Romain\Projet\Suivi_Tarot\config.json", "file"))
