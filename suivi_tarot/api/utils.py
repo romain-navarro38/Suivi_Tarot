bc06acaaf376da6ab93a9954d1f8feb7f7a72fd7
rom4in_n <romain.navarro38@gmail.com>
2022-05-29 11:11:02 +0200

    Add functions manage password

--- suivi_tarot/api/utils.py
+++ suivi_tarot/api/utils.py
@@ -1,9 +1,12 @@
+import hashlib
+import uuid
 from pathlib import Path
 from random import choice
 
 CUR_FILE = Path(__file__)
-DATA_FOLDER = CUR_FILE.parent.parent.parent
-DATA_FILE = DATA_FOLDER / "db.sqlite3"
+ROOT_FOLDER = CUR_FILE.parent.parent.parent
+DATA_FILE = ROOT_FOLDER / "db.sqlite3"
+CONFIG_FILE = ROOT_FOLDER / "config.json"
 IMAGE_FOLDER = CUR_FILE.parent.parent / "ressource" / "image"
 
 HEADER_3_4 = ["Preneur",
@@ -38,7 +41,7 @@ HEADER_6 = ["PNJ",
             "Petit Chelem",
             "Grand Chelem"]
 
-TETE = ["R \u2665",  # cœur
+HEAD = ["R \u2665",  # cœur
         "R \u2666",  # carreau
         "R \u2663",  # trefle
         "R \u2660",  # pique
@@ -51,6 +54,7 @@ PLAYERS = ["Romain", "Ludo", "Emeline", "Eddy", "Aurore"]
 
 COLOR_GRAPH = ['blue', 'darkorange', 'green', 'red', 'purple', 'maroon']
 
+
 def get_random_item_with_constraint(list_str: list[str], prohibited_item: str) -> str:
     # sourcery skip: use-assigned-variable
     """Retourne un élément choisi aléatoirement dans une liste de str
@@ -61,5 +65,25 @@ def get_random_item_with_constraint(list_str: list[str], prohibited_item: str) -
     return item
 
 
+def generate_salt() -> str:
+    """Retourne une chaîne de caractères unique"""
+    return str(uuid.uuid4())
+
+
+def hashage_password(password: str, salt: str = "") -> tuple[str, str]:
+    """Génére le hash d'une chaîne de caractères en lui ajoutant un sel.
+    Retourne le hash et le sel utilisé"""
+    if not salt:
+        salt = generate_salt()
+    hash_ = hashlib.sha256(bytes((password + salt).encode('utf-8'))).hexdigest()
+    return hash_, salt
+
+
+def move_database(path: str):
+    """Déplace la base de données créée dans le dossier projet vers le dossier
+    sélectionné par l'utilisateur"""
+    DATA_FILE.rename(Path(path) / DATA_FILE.name)
+
+
 if __name__ == "__main__":
-    print(get_random_item_with_constraint(PLAYERS, "Romain"))
+    print(hashage_password("romain"))
