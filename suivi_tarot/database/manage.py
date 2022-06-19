a8370390bfa90ea9110ab266b3d157dec3070793
rom4in_n <romain.navarro38@gmail.com>
2022-06-19 22:49:27 +0200

    Added features for managing the colors of the chart of a current game

--- suivi_tarot/database/manage.py
+++ suivi_tarot/database/manage.py
@@ -1,13 +1,14 @@
 import json
 from pathlib import Path
 
-from suivi_tarot.api.utils import CONFIG_FILE
+from suivi_tarot.api.utils import CONFIG_FILE, COLOR_DEFAULT
 
 
 def create_config_file():
-    """Création au niveau du projet du fichier config.json
-    stockant le chemin de la base de données"""
-    set_content_config_database({"path_database": ""})
+    """Creation of the config.json file to store the database path
+    and color list used for the game chart"""
+    set_content_config_database({"path_database": "",
+                                 "player_color": COLOR_DEFAULT})
 
 def get_content_config_database() -> dict:
     """Retourne le contenu de config.json"""
@@ -19,6 +20,21 @@ def set_content_config_database(content: dict):
     """Ecrit le dictionnaire content dans le fichier config.json"""
     with open(CONFIG_FILE, 'w', encoding='utf-8') as f:
         json.dump(content, f, indent=4)
+
+def get_player_color_graph() -> list[str]:
+    """Returns the list of colors used for the game chart"""
+    return get_content_config_database().get("player_color")
+
+def set_player_color_graph(default: bool, color_list: list = None):
+    """Inserts in config.json the list of default colors if default is True
+    otherwise the list passed as the second argument"""
+    content = get_content_config_database()
+    content["player_color"] = COLOR_DEFAULT if default else color_list
+    set_content_config_database(content=content)
+
+def check_presence_player_color_key() -> bool:
+    """Returns true if the player_color key is present in config.json"""
+    return "player_color" in get_content_config_database().keys()
 
 def get_path_database(extension: str) -> tuple[Path, bool]:
     """Retourne le chemin et sa validée de la base de données
@@ -40,6 +56,5 @@ def path_database_is_valid(path: Path, extension: str) -> bool:
 
 
 if __name__ == "__main__":
-    print(CONFIG_FILE.exists())
-    db, is_valid = get_path_database(".sqlite3")
-    print(is_valid)
+    set_player_color_graph(default=False, color_list=['#696969', '#FF8C00', '#008000', '#FF0000', '#800080', '#800000'])
+    print(get_player_color_graph())
